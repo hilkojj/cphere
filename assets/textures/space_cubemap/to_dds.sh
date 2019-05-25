1a0fc7bb0b71777ef78789fe3ffe04ffd08c87a1
hilkojj <hilkojj@outlook.com>
2019-05-25 15:43:26 +0200

    blender space cubemap

---
+++ assets/textures/space_cubemap/to_dds.sh
@@ -0,0 +1,6 @@
+
+for side in front right left top bottom
+do
+    echo $side | cowsay
+    ~/Compressonator_Linux_x86_64_3.1.307/CompressonatorCLI ./assets/textures/space_cubemap/$side.png ./assets/textures/space_cubemap/$side.dds -miplevels 0 -fd DXT3 | lolcat
+done
